0b97e867aff04bea3d4eef85e27e76f989696cf4
pablo <pablo.rodriguezluna@edu.uah.es>
2025-10-14 13:28:34 +0200

    tamano
    tamao

--- Gestor.cpp
+++ Gestor.cpp
@@ -18,9 +18,10 @@ void crearAficionados(int contador){
 	for (int i = 0; i < 10; i++){
 		num[i] = (contador*10 + i + 1);
 	}
+	int tamano = sizeof(num) / sizeof(num[0]);
 	for (int i = 0; i < 10; i++){
-		
-		Aficionado p();
+	
+		Aficionado p(num, tamano);
 		pila.insertar(p)
 	}
 }
