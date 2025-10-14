40dcb6d6c48aa7a71ae7d3b28f0bda8c2004f49e
pablo <pablo.rodriguezluna@edu.uah.es>
2025-10-14 13:24:36 +0200

    Gestor metodos
    Gestor metodos

--- Gestor.cpp
+++ Gestor.cpp
@@ -8,3 +8,19 @@ Gestor::~Gestor()
 {
 }
 
+void eliminarElemento(int num[], int tamaño, int indice){
+	for(int i = indice; i < tamaño - 1; i++){
+		num[i] = num[i + 1]
+	}
+}
+void crearAficionados(int contador){
+	int num[10];
+	for (int i = 0; i < 10; i++){
+		num[i] = (contador*10 + i + 1);
+	}
+	for (int i = 0; i < 10; i++){
+		
+		Aficionado p();
+		pila.insertar(p)
+	}
+}
--- Gestor.hpp
+++ Gestor.hpp
@@ -1,12 +1,16 @@
 #ifndef GESTOR_HPP
 #define GESTOR_HPP
+#include "Aficionado.hpp"
+#include <cstdlib>
 
 class Gestor
 {
 public:
 	Gestor();
 	~Gestor();
-
+	void crearAficionados();
+	void eliminarElemento();
+	int contadorA = 0;
 };
 
 #endif // GESTOR_HPP
