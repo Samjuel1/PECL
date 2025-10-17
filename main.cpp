d77e46bdeb57f9ea4641bb66a60fb20ff5e144fe
Samjuel1 <samuel.garciadios@edu.uah.es>
2025-10-17 21:30:07 +0200

    Cambios en el main
    Ahora el codigo del main esta mas estructurado

--- main.cpp
+++ main.cpp
@@ -18,39 +18,91 @@ int main(int argc, char **argv)
 	Cola cola_soc;
 	Cola cola_sim;
 	Gestor gestor;
+	srand(time(0));
 	
 	do{
-	
-	srand(time(0));
 		cout << "Indique la opcion deseada: " << endl;
 		cin >> opcion;
 		opcion = toupper(opcion);
 		
-/*	switch (opcion){
-		case 'A':
+		
+		
+		switch (opcion){
+			case 'A':
+				for(int i = 1; i < 11; i++){
+					pila.insertar(i - 1);
+				}
+			case 'B':
+				pila.mostrar();
+				break;
+			case 'C':
+				cout << "Vacio" << endl;
+				break;
+			case 'D':
+				for (int i = 1; i < 11; i++){
+					Aficionado aux = pila.extraer();
+					if (aux.getSocio()){
+						cola_soc.insertar(aux);
+					} 
+					else{
+						cola_sim.insertar(aux);
+					}
+				}
+				cout << "Se han insertado los elementos en la cola correctamente. " << endl;
+				break;
+			case 'E':
+				cout << "El contenido de la cola de socios es: " << endl;
+				cola_soc.mostrar();
+				break;
+			case 'F':
+				cout << "El contenido de la cola de simpatizantes es: " << endl;
+				cola_sim.mostrar();
+				break;
+			case 'G':
+				cout << "Vacio" << endl;
+				break;
+			case 'H':
+				cout << "Vacio" << endl;
+				break;
+			case 'I':
+				cout << "Vacio" << endl;
+				break;
+			case 'J':
+				cout << "Vacio" << endl;
+				break;
+			case 'K':
+				system("cls");
+				break;
+				
+				
 			
-		}*/
-		for(int i = 1; i < 11; i++){
-			pila.insertar(i - 1);
-			pila.mostrar();
-		//	break;
-		}	
+				
+				
+			
+			case 'N':
+				for(int i = 1; i < 11; i++){
+					pila.insertar(i - 1);
+					pila.mostrar();
+				}	
 		
 	
 	
-	for (int i = 1; i < 11; i++){
-		Aficionado aux = pila.extraer();
-		if (aux.getSocio()){
-		cola_soc.insertar(aux);
-	} else{
-		cola_sim.insertar(aux);
-	}
-	}
-	cout << endl;
-	cout << "El contenido de la cola de socios es: " << endl;
-	cola_soc.mostrar();
-	cout << "El contenido de la cola de simpatizantes es: " << endl;
-	cola_sim.mostrar();
+				for (int i = 1; i < 11; i++){
+					Aficionado aux = pila.extraer();
+					if (aux.getSocio()){
+						cola_soc.insertar(aux);
+					} 
+					else{
+						cola_sim.insertar(aux);
+					}
+				}
+				cout << endl;
+				cout << "El contenido de la cola de socios es: " << endl;
+				cola_soc.mostrar();
+				cout << "El contenido de la cola de simpatizantes es: " << endl;
+				cola_sim.mostrar();
+				break;
+		}
 	
 	} while (opcion!= 'S');
 	
